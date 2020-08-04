37745fac35abbacc82ea97501fa4d764c1ff5604
Yang Siyu <siyuyang1995@gmail.com>
2020-08-04 08:35:21 +0800

    [yang siyu]feat: should calculate score when meeting a spare

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,6 @@
 public class BowlingGame {
-    public static final int MaxScore = 10;
-    private int[] rolls = new int[21];
+    public static final int MAX_SCORE = 10;
+    private final int[] rolls = new int[21];
     private int rollIndex = 0;
 
     public void roll(int pins) {
@@ -10,21 +10,37 @@ public class BowlingGame {
 
     public int getScore() {
         int score = 0;
-        int roll = 0;
+        int rollIndex = 0;
+        for (int i = 0; i < 10; i++) {
+            if (isStrike(rolls[rollIndex])) {
+                score = calculateStrike(score, rollIndex);
+                rollIndex++;
+            } else if (isSpareInFrame(rollIndex)) {
+                score = calculateSpare(score, rollIndex);
+                rollIndex += 2;
+            } else {
+                score += rolls[rollIndex] + rolls[rollIndex + 1];
+                rollIndex += 2;
+            }
 
-        for (int frame = 0; frame < 10; frame++) {
-            score += rolls[roll] + rolls[roll + 1];
-            roll += 2;
         }
+        return score;
+    }
+    private int calculateStrike(int score, int roll) {
+        score += 10 + rolls[roll + 1] + rolls[roll + 2];
         return score;
     }
 
     private boolean isStrike(int roll) {
-        return rolls[roll] == 10;
+        return roll == MAX_SCORE;
     }
 
-    private boolean isSpare(int roll) {
-        return rolls[roll] + rolls[roll + 1] == 10;
+    private boolean isSpareInFrame(int roll) {
+        return rolls[roll] + rolls[roll + 1] == MAX_SCORE;
     }
 
+    private int calculateSpare(int score, int roll) {
+        score += MAX_SCORE + rolls[roll + 2];
+        return score;
+    }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,22 +1,41 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
     public final BowlingGame game = new BowlingGame();
+
     @Test
     void should_have_zero_score_when_rolling_zero_each_roll() {
         int rolls = 20;
         int pins = 0;
         int expectedScore = 0;
-        rollNtimes(rolls, pins);
+        rollNTimes(rolls, pins);
         assertEquals(game.getScore(), expectedScore);
     }
 
+    @Test
+    void rolling_one_each_role_should_have_twenty_score() {
+        int rolls = 20;
+        int pins = 1;
+        int expectedScore = 20;
+        rollNTimes(rolls, pins);
+        assertEquals(game.getScore(), expectedScore);
+    }
 
-    private void rollNtimes(int times, int pins) {
-        for(int i = 0; i < times; i++) {
+    @Test
+    void should_calculate_score_when_meeting_a_spare() {
+        int expectedScore = 18;
+        game.roll(4);
+        game.roll(6);//spare
+        game.roll(4);
+        rollNTimes(17, 0);
+        assertEquals(game.getScore(), expectedScore);
+    }
+
+    private void rollNTimes(int times, int pins) {
+        for (int i = 0; i < times; i++) {
             game.roll(pins);
         }
     }
-
 }
