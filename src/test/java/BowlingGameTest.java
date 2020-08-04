ee00acf220b006a6acc0da6f64ab745d9146a08f
Yang Siyu <siyuyang1995@gmail.com>
2020-08-04 13:09:04 +0800

    [yang siyu]feat: should calculate score when there is a spare at 10th frame

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -42,10 +42,15 @@ public class BowlingGameTest {
 
     @Test
     void should_calculate_score_when_there_is_a_spare_in_10th_frame() {
-        int expectedScore = 64;
-        int[] rolls = {0, 1, 5, 5, 4, 4, 5, 0, 0, 7, 0, 0, 5, 1, 8, 1, 0, 6, 4, 4};
+        int expectedScore = 71;
+        int[] rolls = {0, 1, 5, 5, 4, 4, 5, 0, 0, 7, 0, 0, 5, 1, 8, 1, 0, 6, 4, 6, 5};
         assertEquals(game.getScore(rolls), expectedScore);
     }
 
-
+    @Test
+    void should_calculate_score_for_normal_scene() {
+        int expectedScore = 64;
+        int[] rolls = {0, 1, 5, 5, 4, 4, 5, 0, 0, 7, 10, 0, 5, 1, 8, 1, 0, 6, 3, 5};
+        assertEquals(game.getScore(rolls), expectedScore);
+    }
 }
